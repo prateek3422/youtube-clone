a2146264ed599c2892961ee9da2dd679b0ecaaa6
prateek3422 <123472716+prateek3422@users.noreply.github.com>
2024-02-16 20:16:43 +0530

    like

--- BACKEND/src/app.js
+++ BACKEND/src/app.js
@@ -23,7 +23,7 @@ import userRouter from "./routes/user.routes.js";
 import videoRouter from "./routes/video.routes.js"
 import subscriptionRouter from "./routes/subscription.routes.js"
 import commentRouter  from "./routes/comment.routes.js";
-
+import likeRouter from "./routes/like.routes.js"
 // router decleartion
 
 app.use("/api/v1/users", userRouter)
@@ -31,7 +31,7 @@ app.use("/api/v1/videos", videoRouter)
 // app.use("/api/v1/tweets", tweetRouter)
 app.use("/api/v1/subscriptions", subscriptionRouter)
 app.use("/api/v1/comments", commentRouter)
-// app.use("/api/v1/likes", likeRouter)
+app.use("/api/v1/likes", likeRouter)
 // app.use("/api/v1/playlist", playlistRouter)
 // app.use("/api/v1/dashboard", dashboardRouter)
 
---
+++ BACKEND/src/controllers/like.controler.js
@@ -0,0 +1,79 @@
+import mongoose, { isValidObjectId } from "mongoose";
+import { Like } from "../models/like.model.js";
+import { ApiError } from "../utils/ApiError.js";
+import { ApiResponse } from "../utils/ApiResponse.js";
+import { asyncHandler } from "../utils/asyncHandler.js";
+
+const toggleVideoLike = asyncHandler(async (req, res) => {
+  const { videoId } = req.params;
+  //TODO: toggle like on video
+  //   console.log(videoId)
+
+  if (!isValidObjectId(videoId)) {
+    throw new ApiError(404, "video not found");
+  }
+
+  const isLike = await Like.find({
+    video: videoId,
+    likedBy: req.user._id,
+  });
+
+  console.log(isLike);
+
+  if (isLike) {
+    const unLike = await Like.findByIdAndDelete(isLike._id);
+    return res.status(200).json(new ApiResponse(200, unLike, "video unlike"));
+  }
+
+  const like = await Like.create({
+    video: videoId,
+    likedBy: req.user._id,
+  });
+
+  // console.log(like)
+  return res.status(200).json(new ApiResponse(200, like, "like successfully"));
+});
+
+const toggleCommentLike = asyncHandler(async (req, res) => {
+  const { commentId } = req.params;
+  //TODO: toggle like on comment
+
+  if (!commentId) {
+    throw new ApiError(404, "comment id  is missing");
+  }
+
+  const iscommentLike = await Comment.find({
+    comment: commentId,
+    likedBy: req.user?._id,
+  });
+
+  if (iscommentLike) {
+    const commentUnlike = await Comment.findByIdAndDelete(commentLike?._id);
+
+    return res
+      .status(200)
+      .json(new ApiResponse(200, commentUnlike, "comment unlike succssfully"));
+  }
+
+  const commentLiked = await Comment.create({
+    comment: commentId,
+    likedBy: req.user?._id,
+  });
+
+  return res
+    .status(200)
+    .json(new ApiResponse(200, commentLiked, "comment like successfully"));
+});
+
+const toggleTweetLike = asyncHandler(async (req, res) => {
+  const { tweetId } = req.params;
+  //TODO: toggle like on tweet
+});
+
+const getLikedVideos = asyncHandler(async (req, res) => {
+  //TODO: get all liked videos
+
+  
+});
+
+export { toggleCommentLike, toggleTweetLike, toggleVideoLike, getLikedVideos };
