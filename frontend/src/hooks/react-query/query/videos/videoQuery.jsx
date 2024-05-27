5003813a8736b3f278264d46d0b51c04e5af7df8
prateek3422 <123472716+prateek3422@users.noreply.github.com>
2024-05-27 13:09:45 +0530

    test 3

--- frontend/src/hooks/react-query/query/videos/videoQuery.jsx
+++ frontend/src/hooks/react-query/query/videos/videoQuery.jsx
@@ -4,7 +4,7 @@ import { useQuery } from "@tanstack/react-query";
 const videoQuery = (slug) => {
   // eslint-disable-next-line react-hooks/rules-of-hooks
   return useQuery({
-    queryKey: ["video"] ,
+    queryKey: [slug] ,
     queryFn: () => api.get(`/api/v1/videos/${slug}`).then((data) =>data?.data?.data),
     staleTime: Infinity,
     refetchOnMount: false,
